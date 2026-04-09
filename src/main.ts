1271c80094b70a84db1484bbfd42051dc4bfde56
Daniel Marreiro <marreiro23@gmail.com>
2026-04-09 09:35:40 -0300

    Gerando modulos com o nest cli

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
-async function inicia() {
+async function App() {
   const aplicativo = await NestFactory.create(AppModule);
   await aplicativo.listen(process.env.PORT ?? 3000); // Inicia o servidor na porta definida em process.env.PORT ou na porta 3000 por padrão
 }
-inicia();
+App();
